6485509078633e69a6a8e9614441fb44c7b3ef32
Francisco Canela <fcanela.dev@gmail.com>
2021-07-18 13:37:10 +0300

    feat(tmux): automatically saves the sessions, windows, panes and adds z for dir jumps

--- prezto/_install.sh
+++ prezto/_install.sh
@@ -3,3 +3,6 @@
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${HOME}/.zprezto"
 
 ln -s ${HOME}/.dotfiles/zdotdir/.zshenv ${HOME}/.zshenv
+
+mkdir ${HOME}/.zprezto-contrib/
+ln -s ${HOME}/.dotfiles/prezto/plugins/zsh-z ${HOME}/.zprezto-contrib/zsh-z
