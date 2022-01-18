26846f1ca64028e29696353d502341755e942cca
Francisco Canela <fcanela.dev@gmail.com>
2022-01-18 15:05:38 +0200

    feat(prezto): updates installation script and plugins

--- prezto/_install.sh
+++ prezto/_install.sh
@@ -6,3 +6,5 @@ ln -s ${HOME}/.dotfiles/zdotdir/.zshenv ${HOME}/.zshenv
 
 mkdir ${HOME}/.zprezto-contrib/
 ln -s ${HOME}/.dotfiles/prezto/plugins/zsh-z ${HOME}/.zprezto-contrib/zsh-z
+
+git clone https://github.com/agkozak/zsh-z.git ~/.zprezto-contrib/zsh-z
