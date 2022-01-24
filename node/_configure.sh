e959de149c4bd426840db31eae09ba433a572ef5
Francisco Canela <fcanela.dev@gmail.com>
2022-01-24 17:38:54 +0200

    feat(node): restores node configuration (removed by mistake)

---
+++ node/_configure.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# This script installs my frequently used npm global packages
+MODULES=(
+  # System tools
+  #----------------------------------------------------------------
+  "tldr"                    # Shorter man manges
+  "fkill"                   # Improved process kill
+  "public-ip-cli"
+  "speed-test"
+
+  # Node.js / Javascript
+  #----------------------------------------------------------------
+  "eslint"                  # ES linter
+  "prettier"                # Prettify JS code
+  "typescript"
+  "npmrc"                   # Handles different npmrc environments
+  "nodemon"                 # JS hotreloading
+  "parcel-bundler"
+
+  # General development
+  #----------------------------------------------------------------
+  "commitizen"              # Git commits convention
+  "cz-conventional-changelog"
+
+  # Other
+  #----------------------------------------------------------------
+  "cash-cli"                # Currency conversion
+  "iponmap"                 # Show IP on map
+  "mapscii"                 # Ascii map
+  "emoj"                    # Search for emojis
+)
+
+npm install -g "${MODULES[@]}"
