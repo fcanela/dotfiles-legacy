5c547b392477ad104d16ff572d05384f704a93f2
Francisco Canela <fcanela.dev@gmail.com>
2022-05-20 14:16:26 +0200

    feat(linux): changes home folder structure

--- ubuntu/configure_directories.sh
+++ ubuntu/configure_directories.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "[+] Configuring directories"
-mkdir -p $HOME/.hidden/{desktop,templates,public} $HOME/{bin,contexts,src/github.com/fcanela,pkg,tmp,media,downloads/www}
+mkdir -p $HOME/.hidden/{desktop,templates,public} $HOME/go/{bin,src,pkg} $HOME/{bin,contexts,cloud,src/github.com/fcanela,tmp,media,downloads/{www,torrent,repo},uncontext}
 rm $HOME/.config/user-dirs.dirs
 ln -s ${HOME}/.dotfiles/ubuntu/user-dirs.dirs ${HOME}/.config/user-dirs.dirs
